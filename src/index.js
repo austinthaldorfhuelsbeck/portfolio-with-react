26bb9b81c9e39620499dbad2cb1e0405d1ea30a8
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-02-06 21:19:54 -0800

    Simplifying file structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SiteNav from "./components/SiteNav/SiteNav";
+import SiteHeader from "./components/SiteHeader/SiteHeader";
+import PortfolioContent from "./components/PortfolioContent/PortfolioContent";
+import SiteFooter from "./components/SiteFooter/SiteFooter";
 import "./index.css";
-import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SiteNav />
+    <SiteHeader />
+    <PortfolioContent />
+    <SiteFooter />
   </React.StrictMode>,
   document.getElementById("root")
 );
