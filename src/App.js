96e79ea7268ef9f8a5b56f28d7c143b1a63c2e7b
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-02-16 17:33:36 -0800

    Add denim jpg

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ export default function App() {
       name: "PawPaw Vintage",
       url: "",
       photoUrl:
-        "https://images.unsplash.com/photo-1560294559-1774a164fb0a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        "https://austinthaldorfhuelsbeck.github.io/portfolio-with-react/img/denim.jpg",
       technologies: ["Shopify", "React"],
     },
   ];
