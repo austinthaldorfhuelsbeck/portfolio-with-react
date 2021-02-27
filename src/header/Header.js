8f6c384ebbf437c657459a08d75b3053beedb895
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-02-27 11:48:40 -0800

    Cleaning up; adding back in data.json

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 
 export default function Header({ pages }) {
   return (
     <div className="header">
-      <NavLink to="/">
+      <Link to="/">
         <hgroup className="header-title">
           <h1>Austin Thaldorf-Huelsbeck</h1>
           <h2>Full-Stack Web Developer</h2>
         </hgroup>
-      </NavLink>
+      </Link>
       <NavBar pages={pages} />
     </div>
   );
