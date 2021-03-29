af56b5ecd8751a63d993cadab7462492fc954bce
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-03-28 20:07:17 -0700

    unsuccessful attempt to link medium

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Blog() {
-  return <div></div>;
+  return (
+    <div>
+      <div id="medium-widget"></div>
+      <script src="https://medium-widget.pixelpoint.io/widget.js"></script>
+      <script>
+        MediumWidget.Init(
+        {{
+          renderTo: "#medium-widget",
+          params: {
+            resource: "https://medium.com/@austinthaldorfhuelsbeck",
+            postsPerLine: 1,
+            limit: 4,
+            picture: "small",
+            fields: ["description", "author", "publishAt"],
+            ratio: "landscape",
+          },
+        }}
+        )
+      </script>
+    </div>
+  );
 }
